2a713d2f7374f1fcf5f91bc747086854a6bef692
Aritra Majumdar <73417041+bear96@users.noreply.github.com>
2023-04-04 15:30:32 -0400

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-wget = /usr/bin/wget
+
 echo "Downloading Berkeley Van Gogh dataset"
 wget "https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/vangogh2photo.zip"
 unzip -q vangogh2photo.zip -d dataset
