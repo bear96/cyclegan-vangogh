38d87195ec21957d06e890be14dbc74f9a427d94
Aritra Majumdar <aritram21@vt.edu>
2023-04-06 10:53:17 -0400

    training more and added scripted model

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import torch
 import torch.nn as nn
-from model import GeneratorResNet
 import torchvision.transforms as transforms
 from PIL import Image
 import io
@@ -12,12 +11,10 @@ t = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5,0.5,0.5)
 
 @st.cache_resource
 def load_model():
-    checkpoint = torch.load("checkpoint/CycleGan_VanGogh_Checkpoint.pt",map_location=torch.device('cpu'))
-    return checkpoint
+    model = torch.jit.load('checkpoint/CycleGAN_Generator.pt')
+    return model
 
-checkpoint = load_model()
-Gen_BA = nn.DataParallel(GeneratorResNet((3,256,256), 10))
-Gen_BA.load_state_dict(checkpoint['Gen_BA'])
+Gen_BA = load_model()
 
 
 def predict(im,Gen_BA):
@@ -27,7 +24,7 @@ def predict(im,Gen_BA):
         height = int(h * scale_factor)
         width = int(w * scale_factor)
         im = transforms.Resize((height,width))(im)
-    st.write(im.size)
+
     input = t(im)
     Gen_BA.eval()
     output = Gen_BA(input.unsqueeze(0))
@@ -38,13 +35,17 @@ st.title(body = "Van Gogh style transfer using CycleGAN.")
 st.markdown("""This app has been built using a machine learning model called CycleGAN. For more information on CycleGAN, 
             you can read this [paper](https://arxiv.org/abs/1703.10593). The dataset used has been sourced from 
             [UC Berkeley](https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/) and other open sourced datasets on Kaggle.
-            If you're interested in the code behind the project, the project is hosted on my [github](https://github.com/bear96/cyclegan-vangogh).
+            If you're interested in the code behind the project, the project is hosted on my [GitHub](https://github.com/bear96/cyclegan-vangogh).
             Alternatively, you can choose to clone into the repository using  
 
             git clone https://github.com/bear96/cyclegan-vangogh.git""")
 st.markdown("""The model hasn't yet been trained to it's limits. It performs poorly on people and objects such as cars. 
 However, the model performs reasonably well on pictures of scenery or nature. The dataset that was used to train the model mostly
 contained pictures of scenery, hence this handicap.""")
+st.warning("""Please note that the app may crash if you provide it with an image that's too large with very high resolution. \
+    Typically, the model seems to perform well on images which are less than or equal to 1MB in size. If you're using this from your phone,
+    it is possible that the model may not work if you provide an image with a .PNG extension. I'm unaware of why this error happens, since
+    if you try to provide the same image via laptop, it works perfectly fine. I'm working on solving this issue.""")
 vg = Image.open("style/Self-Portrait_(1889)_by_Vincent_van_Gogh.jpg")
 vg = vg.resize([int(vg.size[0]*0.2),int(vg.size[1]*0.2)])
 with st.sidebar:
